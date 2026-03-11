45eeb86ace9b2e08dbcbeb878022615c2544787c
lucccxs <luccasamcabral@gmail.com>
2026-03-11 12:09:43 -0300

    Atualização GamePage

--- src/Templates/GamePage/index.jsx
+++ src/Templates/GamePage/index.jsx
@@ -1,23 +1,49 @@
-export default function GamePage({nome, ano, dev, capa, desc, genero, plat, tags, pub, metacritic, tempo, avaliacoes, recomendacoes}){
+import capa from "../../assets/Imagem/jogo.jpeg"
+import { FaPlaystation, FaXbox, FaSteam } from "react-icons/fa";
+import Header from "../../components/header";
+import Recomendado from "../../components/Cards/recomedacao";
+import "./style/style.css"
+
+
+export default function GamePage({nome, ano, dev, desc, genero, pub, metacritic, tempo, avaliacoes, recomendacoes}){
     return(
         <div className="parallax">
             <div className="parallax-image" style={{backgroundImage: 'url()'}}>
                 <div className="overlay"></div>
             </div>
+            <Header/>
             <div className="infos">
-                <h2>caralho de asa</h2>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
+                <div className="header">
+                    <div className="plataformas">
+                        <FaPlaystation size={20}/>
+                        <FaXbox/>
+                        <FaSteam/>
+                        <div className="dev-pub">
+                            <p>Ubisoft</p>
+                            <p>Ubisoft</p>
+                        </div>
+                    </div>
+                </div>
+                <main>
+                    <img src={capa}/>
+                    <p>Assassin's Creed Black Flag</p>
+                    <p>2013</p>
+                    <p>Jogo de piratinha que vira assassino e é fodão, vulgo Edward Kenway</p>
+                    <p>Ação e Aventura</p>
+                    <p>8/10</p>
+                    <p>12h</p>
+                </main>
+                    <div className="recomendados">
+                        <h4>Recomendações</h4>
+                        <div className="recomendacoes">
+                            <Recomendado/>
+                            <Recomendado/>
+                            <Recomendado/>
+                            <Recomendado/>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
 
     )
 }
--- src/Templates/Home/index.jsx
+++ src/Templates/Home/index.jsx
@@ -1,65 +1,14 @@
 import "./style/style.css"
 import GameCard from "../../components/Cards/"
+import Header from "../../components/header"
+
 
 export default function Home(){
 
     return(
         
         <>
-            <header>
-                <div className="pesquisar">
-                    <label>RawgAPI</label>
-                    <input type="text" placeholder="Pesquisar"/>
-                </div>
-                <div className="login">
-                    <button>Entrar</button>
-                    <button>Criar conta</button>
-                </div>
-            </header>
-            <div className="filtros">
-                <li>
-                    <ul>
-                        Plataforma
-                        <button>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
-                            </svg>
-                        </button>
-                    </ul>
-                    <ul>
-                        Gênero
-                        <button>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
-                            </svg>
-                        </button> 
-                    </ul>
-                    <ul>
-                        Desenvolvedores
-                        <button>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
-                            </svg>
-                        </button>
-                    </ul>
-                    <ul>
-                        Ano
-                        <button>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
-                            </svg>
-                        </button> 
-                    </ul>
-                    <ul>
-                        Top
-                        <button>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
-                            </svg>
-                        </button>
-                    </ul>
-                </li>
-            </div>
+            <Header/>
             <div className="pai-cards">
                       <GameCard/>
                       <GameCard/>
